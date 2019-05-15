db0579a202fab58dd4eb9e46ae9ef86a3ad25afe
neilujerdan <julien.letouzic1@gmail.com>
2019-05-15 11:22:27 +0200

    generate CRUD

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -75,4 +75,15 @@ class Tag
 
         return $this;
     }
+	
+	/**
+	 * Generates the magic method
+	 *
+	 */
+	public function __toString(){
+		// to show the name of the Category in the select
+		return $this->name;
+		// to show the id of the Category in the select
+		// return $this->id;
+	}
 }
